48c6cca83c0a994b2ea8c9114b9dc5d24f221c84
Andrew Patterson <andrewpatt7@gmail.com>
2024-01-12 02:17:44 -0500

    mpd-block fixups

--- liate/swaybar/mpd.scm
+++ liate/swaybar/mpd.scm
@@ -10,10 +10,17 @@
   #:use-module (ice-9 match)
   #:use-module (liate goblins)
   #:use-module (liate mpd-client)
-  #:use-module (liate utils macros)
+  #:use-module (liate utils assert)
   #:use-module (liate utils hashmaps)
+  #:use-module (liate utils macros)
   #:use-module (guix progress)
   #:export (^mpd-block))
+
+(define (truncate-str len str)
+  (if (> (string-length str) len)
+      (string-append (string-take str (1- len))
+                     "…")
+      str))
 
 (define (format-current-song cur)
   (if (not cur) "none"
@@ -24,8 +31,14 @@
               (hashmap-ref cur 'grouping
                            (const "")
                            (cute string-append <> ": "))
-              (failure-> (hashmap-ref cur 'title)
-                         (hashmap-ref cur 'file)))))
+              (truncate-str
+               60
+               (failure-> (hashmap-ref cur 'title)
+                          (hashmap-ref cur 'file))))))
+
+(define* (round->int num #:optional (by 1))
+  (assert (integer? by))
+  (inexact->exact (round-quotient num by)))
 
 (define* (^mpd-block bcom cell)
   (define-cell quit? #f)
@@ -34,28 +47,32 @@
 
   (methods
    ((tick)
-    (on (<- mpd 'begin '(status) '(current-song))
-        (match-lambda
-          ((status cur)
-           (<- cell
-               (string-join
-                (cons* (format-current-song cur)
-                       (match (hashmap-ref/default status 'state '?)
-                         ('play "▶")
-                         ('pause "⏸")
-                         ('stop "⏹")
-                         ('? "unk"))
-                       (let/ec ret
-                         (let ((fail (λ () (ret "n/a"))))
-                           (progress-bar (* 100 (/ (hashmap-ref status 'elapsed fail)
-                                                   (hashmap-ref status 'duration fail)))
-                                         10)))
-                       (match (hashmap-ref/default status 'single '?)
-                         (#t (list "s"))
-                         ('oneshot (list "1"))
-                         (_ '())))))))
-        #:catch
-        (lambda (err)
-          (<- cell "Error getting MPD info"))))
+    (unless ($ quit?)
+      (on (<- mpd 'begin '(status) '(current-song))
+          (match-lambda
+            ((status cur)
+             (<- cell
+                 (string-join
+                  (cons* (format-current-song cur)
+                         (match (hashmap-ref/default status 'state '?)
+                           ('play "▶")
+                           ('pause "⏸")
+                           ('stop "⏹")
+                           ('? "unk"))
+                         (let/ec ret
+                           (let* ((fail (λ () (ret "n/a")))
+                                  (elapsed (hashmap-ref status 'elapsed fail))
+                                  (duration (hashmap-ref status 'duration fail)))
+                             (format #f "~a~a"
+                                     (round->int duration 60)
+                                     (progress-bar (* 100 (/ elapsed duration))
+                                                   10))))
+                         (match (hashmap-ref/default status 'single '?)
+                           (#t (list "s"))
+                           ('oneshot (list "1"))
+                           (_ '())))))))
+          #:catch
+          (lambda (err)
+            (<- cell "Error getting MPD info")))))
    ((quit)
     ($ quit? #t))))
